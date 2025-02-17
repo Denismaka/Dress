b62d06efc130c0a77bd67b07caa2441cff28e36b
Denismaka <makadenis370@gmail.com>
2025-02-17 18:35:32 +0100

    Debut du projet

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/font/css/all.min.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="website icon" href="/assets/images/Patmos_Logo_2544cfe63a.webp" />
+    <link rel="stylesheet" href="/assets/swiper/package/swiper-bundle.css">
+    <title>Dress</title>
+</head>
+
+<body>
+
+    <!-- ############## -->
+    <!-- NavBar -->
+    <!-- ############## -->
+
+
+    <!-- ############## -->
+    <!-- Home -->
+    <!-- ############## -->
+
+
+    <!-- ############## -->
+    <!-- About-->
+    <!-- ############## -->
+
+
+    <!-- ############## -->
+    <!-- Services-->
+    <!-- ############## -->
+
+
+    <!-- ############## -->
+    <!-- Projet -->
+    <!-- ############## -->
+
+
+    <!-- ############## -->
+    <!-- Qualité -->
+    <!-- ############## -->
+
+
+    <!-- ############## -->
+    <!-- Testimonials -->
+    <!-- ############## -->
+
+
+    <!-- ################  -->
+    <!-- Contact  -->
+    <!-- ################  -->
+
+
+    <!-- ################  -->
+    <!-- Footer  -->
+    <!-- ################  -->
+
+
+
+
+
+    <script src="/assets/swiper/package/swiper-bundle.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="/assets/scrollreveal/dist/scrollreveal.min.js"></script> -->
+    <script src="/assets/Js/app.js"></script>
+</body>
+
+</html>
