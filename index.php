092daa76216661e60f3f21e094efbddcf98a31dd
Denismaka <makadenis370@gmail.com>
2025-02-17 18:37:12 +0100

    Mise en place du menu de navigation responsive avec bootstrap

--- index.php
+++ index.php
@@ -17,7 +17,45 @@
     <!-- ############## -->
     <!-- NavBar -->
     <!-- ############## -->
-
+    <nav class="navbar navbar-expand-lg" style="z-index: 1000;">
+        <div class="container">
+            <a class="navbar-brand text-uppercase fw-bold" href="#">Dress </a>
+            <button class=" navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <div class="mx-auto">
+                    <ul class="navbar-nav fw-bold">
+                        <li class="nav-item">
+                            <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">About</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Services
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item " style="color: #000;" href="#">Conception et planification</a></li>
+                                <li><a class="dropdown-item " style="color: #000;" href="#">Construction et réalisation</a></li>
+                                <li><a class="dropdown-item " style="color: #000;" href="#">Gestion de chantier</a></li>
+                                <li><a class="dropdown-item " style="color: #000;" href="#">Services aux clients </a></li>
+                                <li><a class="dropdown-item " style="color: #000;" href="#">Expertise et innovation</a></li>
+                                <li><a class="dropdown-item " style="color: #000;" href="#">Autres...</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Projets</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
 
     <!-- ############## -->
     <!-- Home -->
