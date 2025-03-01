405f5b83a60efedfa96ba771d81973074339101d
Denismaka <makadenis370@gmail.com>
2025-03-01 18:11:16 +0100

    Mise en place du script de SrollReveal

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -7,3 +7,14 @@ btn.addEventListener('click', () => {
     behavior: 'smooth',
   });
 });
+
+// ScrollReveal({
+//   reset: true,
+//   distance: '80px',
+//   duration: 2000,
+//   delay: 200,
+// });
+// ScrollReveal().reveal('.container, .heading', { origin: 'top' });
+// ScrollReveal().reveal('img, .col-md-4, form', { origin: 'top' });
+// ScrollReveal().reveal('h1, h3, h4, h5, span, li', { origin: 'top' });
+// ScrollReveal().reveal('p, i, .row', { origin: 'top' });
