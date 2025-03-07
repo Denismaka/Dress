26b34e40a8afd77352f9da604238a6af61191dda
Denismaka <makadenis370@gmail.com>
2025-03-07 16:55:21 +0100

    Mise en place du script de la librairies Js scrollReveal

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -8,13 +8,14 @@ btn.addEventListener('click', () => {
   });
 });
 
-// ScrollReveal({
-//   reset: true,
-//   distance: '80px',
-//   duration: 2000,
-//   delay: 200,
-// });
-// ScrollReveal().reveal('.container, .heading', { origin: 'top' });
-// ScrollReveal().reveal('img, .col-md-4, form', { origin: 'top' });
-// ScrollReveal().reveal('h1, h3, h4, h5, span, li', { origin: 'top' });
-// ScrollReveal().reveal('p, i, .row', { origin: 'top' });
+ScrollReveal({
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200,
+});
+ScrollReveal().reveal('.container, .heading', { origin: 'top' });
+ScrollReveal().reveal('.col-md-4, form', { origin: 'top' });
+ScrollReveal().reveal('h1, h3, h4, h5, h2, span, li', { origin: 'top' });
+ScrollReveal().reveal('p, .row', { origin: 'top' });
+ScrollReveal().reveal('i, a, strong', { origin: 'right' });
