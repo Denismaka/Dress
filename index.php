e1a611fd115dbdf93f3dfc2993ce570f2611d3a1
Denismaka <makadenis370@gmail.com>
2025-03-07 16:23:16 +0100

    changement de phrases d'accroche dans la newsletter et dans le copiryght tous dans le footer

--- index.php
+++ index.php
@@ -365,8 +365,8 @@
                         </div>
                         <div class="col-md-4 mb-3">
                             <form>
-                                <h5>Subscribe to our newsletter</h5>
-                                <p>Monthly digest of what's new and exciting from us.</p>
+                                <h5>Abonnez-vous à notre newsletter</h5>
+                                <p>Résumé mensuel de ce qui est nouveau et passionnant chez nous.</p>
                                 <div class="d-flex flex-column flex-sm-row w-100 gap-2">
                                     <label for="newsletter1" class="visually-hidden">Email address</label>
                                     <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
@@ -377,7 +377,7 @@
                     </div>
 
                     <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-                        <p>&copy; 2024 Denis maka, All rights reserved.</p>
+                        <p>&copy; 2025 <a href="#" class="text-decoration-none text-white">Denis maka</a>, Tous droits réservés.</p>
                         <ul class="list-unstyled d-flex">
                             <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24">
                                         <use xlink:href="#twitter" />
