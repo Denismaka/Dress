31206d691545bd3b68d1ede2ccefb183809462e0
Denismaka <makadenis370@gmail.com>
2025-05-22 00:30:03 +0100

    correction du scoll

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -1,21 +1,21 @@
-const btn = document.querySelector('.arrow');
+const btn = document.querySelector(".arrow");
 
-btn.addEventListener('click', () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth',
-  });
+btn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
 });
 
 ScrollReveal({
-  reset: true,
-  distance: '80px',
-  duration: 2000,
-  delay: 200,
+    reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
 });
-ScrollReveal().reveal('.container, .heading', { origin: 'top' });
-ScrollReveal().reveal('.col-md-4, form', { origin: 'top' });
-ScrollReveal().reveal('h1, h3, h4, h5, h2, span, li', { origin: 'top' });
-ScrollReveal().reveal('p, .row', { origin: 'top' });
-ScrollReveal().reveal('i, a, strong', { origin: 'right' });
+ScrollReveal().reveal(".container, .heading", { origin: "top" });
+ScrollReveal().reveal(".col-md-4, form", { origin: "top" });
+ScrollReveal().reveal("h1, h3, h4, h5, h2, span, li", { origin: "top" });
+ScrollReveal().reveal("p, .row", { origin: "top" });
+ScrollReveal().reveal("i, a, strong", { origin: "top" });
